2e687b56086edff76852931aea3ca75e453fed1c
jsntn12 <jsantana@nperts-MacBook-Pro.local>
2023-04-25 15:24:47 -0400

    feat: Converte temperature from C to F and vice versa

--- convert.py
+++ convert.py
@@ -16,6 +16,19 @@ def convert_temp(unit_in, unit_out, temp):
     """
 
     # YOUR CODE HERE
+    if unit_in == "c" and unit_out == "f":
+        return (9/5 * temp) + 32
+
+    elif unit_in == "f" and unit_out == "c":
+        return 5/9 * (temp - 32)
+
+    elif unit_in not in ["c", "f"]:
+        return f"Invalid unit {unit_in}"
+
+    elif unit_out not in ["c", "f"]:
+        return f"Invalid unit {unit_out}"
+
+    return temp
 
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
@@ -23,4 +36,3 @@ print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
 print("z", "f", 32, convert_temp("z", "f", 32), "should be Invalid unit z")
 print("c", "z", 32, convert_temp("c", "z", 32), "should be Invalid unit z")
 print("f", "f", 75.5, convert_temp("f", "f", 75.5), "should be 75.5")
-
